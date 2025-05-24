30fc2569a477021d6cb4ae708e20d379d3407341
yaswanthkumar <yashyaswanth714@gmail.com>
2025-05-24 15:17:07 +0530

    Initial commit: Task-3 PHP/MySQL blog application with CRUD, authentication, search, pagination, and Bootstrap UI

---
+++ includes/config.example.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'blog_app');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('SITE_NAME', 'Professional Blog');
+define('POSTS_PER_PAGE', 5);
+define('BASE_PATH', 'C:/xampp/htdocs/Internship-Tasks-for-Web-Development-PHP-MySQL/Task-3/');
+define('BASE_URL', 'http://localhost/Internship-Tasks-for-Web-Development-PHP-MySQL/Task-3/');
+session_start();
